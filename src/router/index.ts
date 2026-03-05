05e127da3d73a7487a525424fe0db434829b79e3
takahashiyb <takahashiyb89@gmail.com>
2026-03-05 08:33:19 +0800

    Set up the vue router and all the pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,17 @@
+import BookmarksPage from '@/pages/BookmarksPage.vue'
+import HomePage from '@/pages/HomePage.vue'
+import MoviesPage from '@/pages/MoviesPage.vue'
+import TvPage from '@/pages/TvPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { name: 'home', component: HomePage, path: '/' },
+    { name: 'movies', component: MoviesPage, path: '/movies' },
+    { name: 'tvseries', component: TvPage, path: '/tvseries' },
+    { name: 'bookmarks', component: BookmarksPage, path: '/bookmarks' },
+  ],
 })
 
 export default router
