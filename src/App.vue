995c9ba626b8232ba1827b5fafa8a6c2b4405622
takahashiyb <takahashiyb89@gmail.com>
2026-03-05 08:35:55 +0800

    Add the header which also includes the navbar component.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavBar from './components/NavBar.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main>
+    <header>
+      <img src="@/assets/icons/logo.svg" alt="logo" />
+      <NavBar />
+      <div>
+        <img
+          class="avatar"
+          src="/public/assets/images/avatars/image-avatar.png"
+          alt="profile-avatar"
+        />
+      </div>
+    </header>
+    <RouterView> </RouterView>
+  </main>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@import '@/assets/styles/main.scss';
+
+header {
+  background-color: rgba($blue-900, 1);
+  height: $spacing-0700;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: stretch;
+
+  padding: $spacing-0300;
+  padding-inline: $spacing-0200;
+}
+
+img {
+  height: 100%;
+  aspect-ratio: 1;
+}
+
+.avatar {
+  border: solid rgba($white, 1) 2px;
+  border-radius: 50%;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,80 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const isSelected = ref('movies')
+
+function switchNavHighlight(current: string) {
+  isSelected.value = current
+}
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li>
+        <RouterLink :to="{ name: 'home' }">
+          <img
+            src="/src/assets/icons/icon-nav-home.svg"
+            alt="nav icon for home"
+            :class="{ selected: isSelected === 'home' }"
+            @click="switchNavHighlight('home')"
+          />
+        </RouterLink>
+      </li>
+      <li>
+        <RouterLink :to="{ name: 'movies' }">
+          <img
+            src="/src/assets/icons/icon-nav-movies.svg"
+            alt="nav icon for movies"
+            :class="{ selected: isSelected === 'movies' }"
+            @click="switchNavHighlight('movies')"
+          />
+        </RouterLink>
+      </li>
+      <li>
+        <RouterLink :to="{ name: 'tvseries' }">
+          <img
+            src="/src/assets/icons/icon-nav-tv-series.svg"
+            alt="nav icon for tv series"
+            :class="{ selected: isSelected === 'tvseries' }"
+            @click="switchNavHighlight('tvseries')"
+          />
+        </RouterLink>
+      </li>
+      <li>
+        <RouterLink :to="{ name: 'bookmarks' }">
+          <img
+            src="/src/assets/icons/icon-nav-bookmark.svg"
+            alt="nav icon for bookmarks"
+            :class="{ selected: isSelected === 'bookmarks' }"
+            @click="switchNavHighlight('bookmarks')"
+          />
+        </RouterLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/main.scss';
+
+nav > ul {
+  height: 100%;
+  list-style-type: none;
+
+  display: flex;
+  gap: $spacing-0300;
+}
+
+nav ul li img {
+  height: 100%;
+}
+
+nav ul li img:hover {
+  filter: saturate(100) brightness(80) invert(0.77);
+}
+
+nav ul li img.selected {
+  filter: brightness(100);
+}
+</style>
