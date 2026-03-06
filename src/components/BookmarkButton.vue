431a14f3b016787980b53cd3da7cde8d523667ca
takahashiyb <takahashiyb89@gmail.com>
2026-03-06 10:16:31 +0800

    Create Bookmark button component

---
+++ src/components/BookmarkButton.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+const isFull = false
+</script>
+<template>
+  <button>
+    <img src="@/assets/icons/icon-bookmark-full.svg" alt="" v-if="isFull" />
+    <img src="@/assets/icons/icon-bookmark-empty.svg" alt="" v-else />
+  </button>
+</template>
+
+<style scoped lang="scss">
+@use '@/assets/styles/main.scss' as v;
+
+button {
+  display: grid;
+  place-content: center;
+  height: 32px;
+  width: 32px;
+  background-color: rgba(v.$blue-950, 50%);
+
+  border-radius: 50%;
+  border: none;
+
+  position: absolute;
+  top: 16px;
+  right: 16px;
+}
+</style>
