a7dfdeaaf2f905eadca5cc4cba713bbcde4a3861
takahashiyb <takahashiyb89@gmail.com>
2026-03-06 15:03:46 +0800

    Set up HomePage page component

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,2 +1,26 @@
-<script setup lang="ts"></script>
-<template>hello</template>
+<script setup lang="ts">
+import CardGroup from '@/components/CardGroup.vue'
+import TrendingGroup from '@/components/TrendingGroup.vue'
+</script>
+<template>
+  <section>
+    <h2>Trending</h2>
+    <div><TrendingGroup /></div>
+    <h2>Recommended</h2>
+    <div><CardGroup /></div>
+  </section>
+</template>
+<style lang="scss" scoped>
+@use '@/assets/styles/main.scss' as v;
+section {
+  padding: v.$spacing-0200;
+}
+
+h2 {
+  padding-bottom: v.$spacing-0200;
+}
+
+div {
+  padding-bottom: v.$spacing-0300;
+}
+</style>
