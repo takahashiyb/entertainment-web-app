4d0fcd1caf9f00583b3d908ae1f401a0c5982326
takahashiyb <takahashiyb89@gmail.com>
2026-03-06 15:02:28 +0800

    Change the createWebHistory to createWebHashHistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,10 @@ import BookmarksPage from '@/pages/BookmarksPage.vue'
 import HomePage from '@/pages/HomePage.vue'
 import MoviesPage from '@/pages/MoviesPage.vue'
 import TvPage from '@/pages/TvPage.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { name: 'home', component: HomePage, path: '/' },
     { name: 'movies', component: MoviesPage, path: '/movies' },
