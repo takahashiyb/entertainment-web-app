d1ec51fddebfd5d117bfb54301a9eb13954b6671
takahashiyb <takahashiyb89@gmail.com>
2026-03-06 15:00:29 +0800

    Set up the trending section component

---
+++ src/components/TrendingGroup.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useDataStore } from '@/stores/data'
+import MediaCard from './MediaCard.vue'
+
+const data = useDataStore()
+</script>
+<template>
+  <div class="container">
+    <MediaCard
+      v-for="media in data.json?.filter((media) => media.isTrending)"
+      :key="media.title.trim()"
+      :mediaData="media"
+      cardType="trending"
+    />
+  </div>
+</template>
+<style scoped lang="scss">
+@use '@/assets/styles/main.scss' as v;
+
+.container {
+  overflow-x: auto;
+  overflow-y: hidden;
+  scrollbar-width: none;
+
+  display: flex;
+  align-items: stretch;
+  gap: v.$spacing-0200;
+}
+
+.container > * {
+  height: 140px;
+  width: 240px;
+}
+</style>
